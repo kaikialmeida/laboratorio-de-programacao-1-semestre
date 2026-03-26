3e3b31c73710aaa8546182aa2659291e2da49108
kaikialmeida <kaikialmeida02@gmail.com>
2026-03-26 18:26:16 -0300

    Add files via upload

---
+++ bonus de vendas.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	
+	int v = 8000, t = 6;
+	if (v > 10000){
+		if (t >= 5){
+        	printf ("bonus alto\n");
+        	
+		}
+	}
+	else { 
+	printf ("bonus padrao\n ");
+	}
+	system ("pause");
+}
